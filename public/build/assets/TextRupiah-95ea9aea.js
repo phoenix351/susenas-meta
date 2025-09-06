ba33ceede4d6e08facec78a194b6af718777a486
phoenix351 <kuat.ponimin123@gmail.com>
2025-09-07 08:17:36 +0900

    build

--- public/build/assets/TextRupiah-95ea9aea.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./app-79131a43.js";import{S as i}from"./index-299793b7.js";import{T as p}from"./index-ff8bac63.js";const{Text:o}=p,d=({value:t,color:r})=>e(i,{style:{textAlign:"right",justifyContent:"end"},children:e(o,{style:{color:r,paddingLeft:"11px",paddingRight:"11px",whiteSpace:"nowrap"},children:t>0?`Rp ${Number(t).toLocaleString("id")}`:""})});export{d as T};
